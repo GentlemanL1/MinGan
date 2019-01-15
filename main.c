4f1d28e208adecd53102f9585d4184447062631f
git <65435748@qq.com>
2019-01-15 21:24:42 +0800

    增加增添删除功能

--- main.c
+++ main.c
@@ -1,9 +1,66 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+         char arr[1024];
+         int index = 0;
+        printf("敏感字符管理系统\n");
 
-    return 0;
+        while (1)
+        {
+            printf("1---添加敏感字符\n");
+            printf("2---删除敏感字符（最后一个）\n");
+            printf("3---查看所有敏感字符\n");
+            printf("4---替换内容中的敏感字符\n");
+            printf("5---退出\n");
 
-}
+            printf("请选择相应功能\n");
+
+            int code;
+            scanf("%d", &code);
+
+            if (code == 1)
+            {
+                printf("请输入要添加的敏感字符\n");
+                char m;
+                scanf("%c", &m);
+                scanf("%c", &m);
+
+                arr[index] = m;
+                index++;
+
+                printf("添加成功，点击回车继续\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
+
+            if (code == 2)
+            {
+
+                printf("删除最后一个敏感字符\n");
+                index--;
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+
+                printf("删除成功，点击回车继续\n");
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
+            if (code == 3)
+            {
+            }
+            if (code == 4)
+            {
+            }
+            if (code == 5)
+            {
+                printf("退出系统\n");
+                break;
+            }
+        }
+
+        return 0;
+    }
